6a68cad0e63fcfba06f3f3d3612769c2ac446022
Vishal Tiwari <123157888+vishaltiwari012@users.noreply.github.com>
2024-02-03 23:22:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV != 'production') {
 require('dotenv').config();
 }
-const express = require('express');
+const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
